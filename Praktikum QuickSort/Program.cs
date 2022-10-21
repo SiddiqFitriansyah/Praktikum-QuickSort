7c82d2ddf7d6f05575cf9427f0548e9bbf1f2274
SiddiqFitriansyah <siddiqfitriansyah14@gmail.com>
2022-10-21 16:55:18 +0700

    Add project files.

---
+++ Praktikum QuickSort/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Praktikum_QuickSort
+{
+    class Program
+    {
+        //Array of integer to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //Number of compression
+        private int new_count = 0; // Number of data movements
+
+    }
+}
